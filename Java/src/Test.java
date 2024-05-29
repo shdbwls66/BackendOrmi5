1f74347329ea51756c5a607efd56ecfd14797117
노유진 <shdbwls66@naver.com>
2024-05-29 19:38:14 +0900

    Java
    Java
    
    Signed-off-by: 노유진 <shdbwls66@naver.com>

---
+++ Java/src/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+
+    /**
+    * @author 노유진
+     * @see Test
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("sout 엔터!");
+        System.out.println(1);
+    }
+}
